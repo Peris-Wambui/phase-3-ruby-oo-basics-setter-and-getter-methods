4fcc4d12df3bbd5f5ae8c38666126c51b6bc4d1a
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-10-24 12:41:24 -0400

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+    def name=(name)
+        @name = name 
+    end 
 
-end
+    def name
+        @name
+      end
+
+    def breed= (breed)
+        @breed = breed
+    end
+
+    def breed 
+        @breed
+    end
+
+    #   fido = Dog.new
+    #   fido.name ="fido"
+
+    #   snoopy.breed =Dog.new
+    #   snoopy.breed ="Beagle"
+
+end 
